37113af27d3c0da6746f32b30bd846e0ec79b5c5
sumanthsvjfrog <sumanthsv@jfrog.com>
2025-10-23 23:19:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email='jfrog@jfrog.com',
     url='https://github.com/jfrog/project-examples',
     packages=['pythonExample'],
-    install_requires=['PyYAML>3.11', 'nltk','Django==2.2.0'],
+    install_requires=['PyYAML==3.11', 'nltk','Django==2.2.0'],
 )
